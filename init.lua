a78c86765e5331121c9ceff21ad8680cf0d81222
wenzhe <mysteryven@gmail.com>
2024-05-01 16:47:04 +0800

    add copilot

--- init.lua
+++ init.lua
@@ -606,6 +606,19 @@ require('lazy').setup({
 
   { -- Autoformat
     'stevearc/conform.nvim',
+    event = { 'BufWritePre' },
+    cmd = { 'ConformInfo' },
+    keys = {
+      {
+        -- Customize or remove this keymap to your liking
+        '<leader>f',
+        function()
+          require('conform').format { async = true, lsp_fallback = true }
+        end,
+        mode = '',
+        desc = 'Format buffer',
+      },
+    },
     opts = {
       notify_on_error = false,
       format_on_save = function(bufnr)
---
+++ lua/custom/plugins/copilot.lua
@@ -0,0 +1,5 @@
+return {
+  'github/copilot.vim',
+  -- use opts = {} for passing setup options
+  -- this is equalent to setup({}) function
+}
