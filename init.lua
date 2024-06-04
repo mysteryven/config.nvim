4018f89d754592a56aa35a21c644b1ce8eba5653
wenzhe <mysteryven@gmail.com>
2024-06-04 18:33:53 +0800

    udpate

--- init.lua
+++ init.lua
@@ -1,5 +1,4 @@
 --[[
-
 =====================================================================
 ==================== READ THIS BEFORE CONTINUING ====================
 =====================================================================
@@ -202,6 +201,16 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 
 vim.keymap.set('n', '<C-n>', ':Neotree position=right reveal <CR>', { desc = 'Open Neotree' })
 
+-- custom keymaps:
+-- set ctrl + L to accpet one word in copilot
+vim.keymap.set('i', '<C-l>', '<C-Space><C-n>', { desc = 'Accept one word in copilot' })
+vim.keymap.set('i', '<C-L>', '<Plug>(copilot-accept-word)', { desc = 'Accept one word in copilot' })
+
+require '/lua/custom/user/switch_case'
+vim.api.nvim_set_keymap('n', '<Leader>cu', '<cmd>lua require("/lua/custom/user/switch_case").switch_case()<CR>', {
+  desc = 'switch CamelCase/snake_case',
+})
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
@@ -268,6 +277,35 @@ require('lazy').setup({
         changedelete = { text = '~' },
       },
       current_line_blame = true,
+      on_attach = function(bufnr)
+        local function map(mode, lhs, rhs, opts)
+          opts = vim.tbl_extend('force', { noremap = true, silent = true }, opts or {})
+          vim.api.nvim_buf_set_keymap(bufnr, mode, lhs, rhs, opts)
+        end
+
+        -- Navigation
+        map('n', ']c', "&diff ? ']c' : '<cmd>Gitsigns next_hunk<CR>'", { expr = true })
+        map('n', '[c', "&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>'", { expr = true })
+
+        -- Actions
+        map('n', '<leader>hs', ':Gitsigns stage_hunk<CR>')
+        map('v', '<leader>hs', ':Gitsigns stage_hunk<CR>')
+        map('n', '<leader>hr', ':Gitsigns reset_hunk<CR>')
+        map('v', '<leader>hr', ':Gitsigns reset_hunk<CR>')
+        map('n', '<leader>hS', '<cmd>Gitsigns stage_buffer<CR>')
+        map('n', '<leader>hu', '<cmd>Gitsigns undo_stage_hunk<CR>')
+        map('n', '<leader>hR', '<cmd>Gitsigns reset_buffer<CR>')
+        map('n', '<leader>hp', '<cmd>Gitsigns preview_hunk<CR>')
+        map('n', '<leader>hb', '<cmd>lua require"gitsigns".blame_line{full=true}<CR>')
+        map('n', '<leader>tb', '<cmd>Gitsigns toggle_current_line_blame<CR>')
+        map('n', '<leader>hd', '<cmd>Gitsigns diffthis<CR>')
+        map('n', '<leader>hD', '<cmd>lua require"gitsigns".diffthis("~")<CR>')
+        map('n', '<leader>td', '<cmd>Gitsigns toggle_deleted<CR>')
+
+        -- Text object
+        map('o', 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+        map('x', 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+      end,
     },
   },
 
@@ -353,6 +391,8 @@ require('lazy').setup({
       -- This opens a window that shows you all of the keymaps for the current
       -- Telescope picker. This is really useful to discover what Telescope can
       -- do as well as how to actually do it!
+      --
+      local actions = require 'telescope.actions'
 
       -- [[ Configure Telescope ]]
       -- See `:help telescope` and `:help telescope.setup()`
@@ -362,9 +402,15 @@ require('lazy').setup({
         --
         defaults = {
           mappings = {
-            i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-            -- press ctrl + k to close current buffer like :bd
-            n = { ['<c-k>'] = require('telescope.actions').close },
+            i = {
+              ['<c-enter>'] = 'to_fuzzy_refine',
+              ['<c-S-d>'] = actions.delete_buffer,
+              ['<C-j>'] = actions.cycle_history_next,
+              ['<C-k>'] = actions.cycle_history_prev,
+            },
+            n = {
+              ['dd'] = actions.delete_buffer,
+            },
           },
         },
         pickers = {
@@ -623,7 +669,7 @@ require('lazy').setup({
     keys = {
       {
         -- Customize or remove this keymap to your liking
-        '<leader>f',
+        '<leader>cf',
         function()
           require('conform').format { async = true, lsp_fallback = true }
         end,
---
+++ lua/custom/plugins/bqf.lua
@@ -0,0 +1,5 @@
+return {
+    'kevinhwang91/nvim-bqf',
+    -- use opts = {} for passing setup options
+    -- this is equalent to setup({}) function
+}
