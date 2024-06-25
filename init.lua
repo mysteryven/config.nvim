1f95559bc3f22d60752c89337fed612b99ffb8a0
wenzhe <mysteryven@gmail.com>
2024-06-25 17:25:29 +0800

    update

--- init.lua
+++ init.lua
@@ -338,6 +338,9 @@ require('lazy').setup({
         ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
         ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
         ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+        ['<leader>h'] = { name = '[H]unk', _ = 'which_key_ignore' },
+        ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
+        ['<leader>g'] = { name = '[G]et', _ = 'which_key_ignore' },
       }
     end,
   },
