d4df18e1257ff068fad8d8239fe5ac5cd69f415c
wenzhe <mysteryven@gmail.com>
2024-12-12 21:19:18 +0800

    upgrad rustaceam

--- lua/custom/plugins/rustaceanvim.lua
+++ lua/custom/plugins/rustaceanvim.lua
@@ -1,45 +1,6 @@
 return {
   'mrcjkb/rustaceanvim',
-  version = '^4', -- Recommended
+  version = '^5', -- Recommended
   lazy = false, -- This plugin is already lazy
-  config = function()
-    require('lspconfig').rust_analyzer.setup {
-      settings = {
-        ['rust-analyzer'] = {
-          inlayHints = {
-            bindingModeHints = {
-              enable = false,
-            },
-            chainingHints = {
-              enable = true,
-            },
-            closingBraceHints = {
-              enable = true,
-              minLines = 25,
-            },
-            closureReturnTypeHints = {
-              enable = 'never',
-            },
-            lifetimeElisionHints = {
-              enable = 'never',
-              useParameterNames = false,
-            },
-            maxLength = 25,
-            parameterHints = {
-              enable = true,
-            },
-            reborrowHints = {
-              enable = 'never',
-            },
-            renderColons = true,
-            typeHints = {
-              enable = true,
-              hideClosureInitialization = false,
-              hideNamedConstructor = false,
-            },
-          },
-        },
-      },
-    }
-  end,
+  config = function() end,
 }
