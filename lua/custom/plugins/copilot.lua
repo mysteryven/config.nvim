3561519bd742c3f093563d9b81a716dd68543754
mysteryven <mysteryven@gmail.com>
2025-10-09 13:57:18 +0800

    update

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -3,6 +3,22 @@ return {
     'github/copilot.vim',
   },
   {
+    'folke/sidekick.nvim',
+    keys = {
+      {
+        '<tab>',
+        function()
+          -- if there is a next edit, jump to it, otherwise apply it if any
+          if not require('sidekick').nes_jump_or_apply() then
+            return '<Tab>' -- fallback to normal tab
+          end
+        end,
+        expr = true,
+        desc = 'Goto/Apply Next Edit Suggestion',
+      },
+    },
+  },
+  {
     'zbirenbaum/copilot.lua',
     cmd = 'Copilot',
     event = 'InsertEnter',
